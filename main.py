f210b850e3fe354668fbf1570701520a3eecd68e
satbir.singh@planetinnovation.com.au <satbir.singh@planetinnovation.com.au>
2023-07-08 13:34:41 +1000

    add pyqt, added slider, working

--- main.py
+++ main.py
@@ -1,102 +1,83 @@
 import vtk
+from PyQt5 import QtCore, QtGui, QtWidgets
+from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
 
-class MeasureInteractorStyle(vtk.vtkInteractorStyleTrackballCamera):
-    def __init__(self, renderer):
-        self.AddObserver("KeyPressEvent", self.on_key_press)
-        self.AddObserver("KeyReleaseEvent", self.on_key_release)
-        self.AddObserver("LeftButtonPressEvent", self.on_left_button_press)
-        self.AddObserver("LeftButtonReleaseEvent", self.on_left_button_release)
-        self.renderer = renderer
-        self.line_source = vtk.vtkLineSource()
-        self.line_mapper = vtk.vtkPolyDataMapper()
-        self.line_mapper.SetInputConnection(self.line_source.GetOutputPort())
-        self.line_actor = vtk.vtkActor()
-        self.line_actor.SetMapper(self.line_mapper)
-        self.renderer.AddActor(self.line_actor)
-        self.start_point = [0, 0, 0]
-        self.end_point = [0, 0, 0]
-        self.alt_pressed = False
-        self.text_actor = vtk.vtkTextActor()
-        self.text_actor.SetPosition(10, 10)
-        self.text_actor.GetTextProperty().SetFontSize(18)
-        self.renderer.AddActor2D(self.text_actor)
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
 
-    def on_key_press(self, obj, event):
-        key = self.GetInteractor().GetKeySym()
-        if key == "Alt_L":
-            self.alt_pressed = True
+        # Create a VTK widget
+        self.vtk_widget = QVTKRenderWindowInteractor(self)
 
-    def on_key_release(self, obj, event):
-        key = self.GetInteractor().GetKeySym()
-        if key == "Alt_L":
-            self.alt_pressed = False
+        # Create a slider widget
+        self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal, self)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(100)
+        self.slider.setValue(100)
+        self.slider.setTickInterval(10)
+        self.slider.setTickPosition(QtWidgets.QSlider.TicksBelow)
 
-    def on_left_button_press(self, obj, event):
-        if self.alt_pressed:
-            click_pos = self.GetInteractor().GetEventPosition()
-            picker = vtk.vtkCellPicker()
-            picker.Pick(click_pos[0], click_pos[1], 0, self.renderer)
-            self.start_point = picker.GetPickPosition()
+        # Create the layout
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.vtk_widget)
+        layout.addWidget(self.slider)
 
-    def on_left_button_release(self, obj, event):
-        if self.alt_pressed:
-            click_pos = self.GetInteractor().GetEventPosition()
-            picker = vtk.vtkCellPicker()
-            picker.Pick(click_pos[0], click_pos[1], 0, self.renderer)
-            self.end_point = picker.GetPickPosition()
+        # Create a frame and set the layout
+        frame = QtWidgets.QFrame(self)
+        frame.setLayout(layout)
 
-            distance = vtk.vtkMath.Distance2BetweenPoints(self.start_point, self.end_point)
-            self.text_actor.SetInput(f"Distance: {distance:.2f}")
+        # Set the central widget
+        self.setCentralWidget(frame)
 
-            self.line_source.SetPoint1(self.start_point)
-            self.line_source.SetPoint2(self.end_point)
-            self.line_source.Update()
+        # Initialize VTK objects and rendering
+        self.renderer = vtk.vtkRenderer()
+        self.vtk_widget.GetRenderWindow().AddRenderer(self.renderer)
+        self.interactor = self.vtk_widget.GetRenderWindow().GetInteractor()
 
-            self.GetInteractor().GetRenderWindow().Render()
+        # Load and render the STL file
+        self.load_stl_file('path_to_stl_file.stl')
 
+        # Connect the slider value change event
+        self.slider.valueChanged.connect(self.on_slider_value_changed)
 
-def render_stl_file(file_path):
-    # Create a reader and load the STL file
-    reader = vtk.vtkSTLReader()
-    reader.SetFileName(file_path)
-    reader.Update()
+    def load_stl_file(self, file_path):
+        # Create a reader and load the STL file
+        reader = vtk.vtkSTLReader()
+        reader.SetFileName(file_path)
+        reader.Update()
 
-    # Get the STL mesh
-    stl_mesh = reader.GetOutput()
+        # Get the STL mesh
+        stl_mesh = reader.GetOutput()
 
-    # Create a mapper
-    mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputData(stl_mesh)
+        # Create a mapper
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputData(stl_mesh)
 
-    # Create an actor
-    actor = vtk.vtkActor()
-    actor.SetMapper(mapper)
+        # Create an actor
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
 
-    # Create a renderer and add the actor
-    renderer = vtk.vtkRenderer()
-    renderer.AddActor(actor)
+        # Add the actor to the renderer
+        self.renderer.AddActor(actor)
+        self.renderer.ResetCamera()
+        self.vtk_widget.GetRenderWindow().Render()
 
-    # Create a render window
-    render_window = vtk.vtkRenderWindow()
-    render_window.AddRenderer(renderer)
-
-    # Create an interactor
-    interactor = vtk.vtkRenderWindowInteractor()
-    interactor.SetRenderWindow(render_window)
-
-    # Set the custom interactor style
-    measure_style = MeasureInteractorStyle(renderer)
-    interactor.SetInteractorStyle(measure_style)
-
-    # Initialize the interactor and start the rendering loop
-    interactor.Initialize()
-    render_window.Render()
-
-    # Start the interactor loop
-    interactor.Start()
+    def on_slider_value_changed(self, value):
+        opacity = value / 100.0
+        actors = self.renderer.GetActors()
+        actors.InitTraversal()
+        actor = actors.GetNextItem()
+        while actor:
+            actor.GetProperty().SetOpacity(opacity)
+            actor = actors.GetNextItem()
+        self.vtk_widget.GetRenderWindow().Render()
 
 
-if __name__ == "__main__":
-    file_path = 'path_to_stl_file.stl'
-    render_stl_file(file_path)
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    window.resize(800, 600)
+    sys.exit(app.exec_())
